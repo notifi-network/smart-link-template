5b64cfbdf725813c3d29fa71d3fec2327c18012c
mayu <mayuran.ganesathas@gmail.com>
2025-04-04 22:22:02 -0700

    update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # smart-link-template
 Smart Link Template
+
+
+This is a generated template based on your config from Notifi Admin Panels
+
---
+++ index.ts
@@ -0,0 +1,22 @@
+// -api kai is working to upload action
+
+// -api to pull schema
+
+// passedObject= {
+//     contextID: string
+//     parameters: {
+//         BS
+//     }
+// }
+
+// parseEntry (passedObject: passedObject) {
+
+// validateInput (compares schema for input and output )
+
+// validateResponse
+
+//     return JSON OBJECT
+
+// }
+
+// rollup actual file  s
