f678fc20c9b38677ca8cce5a14fb959e7303aefe
mayu <mayuran.ganesathas@gmail.com>
2025-04-10 15:56:23 -0700

    update tests

--- tests/unit/index.test.ts
+++ tests/unit/index.test.ts
@@ -7,18 +7,16 @@ console.log(
 describe("Expected Function Output From Action", () => {
   it("should return either SmartLinkActionResult or SmartLinkUserTxInteraction", () => {
     const result = parse();
-    expect(result).toBeInstanceOf(Array);
-    result.forEach((item) => {
-      expect(item).toHaveProperty("successMessage");
-      expect(item).toHaveProperty("failureMessage");
+    expect(result).toHaveProperty("successMessage");
+    expect(result).toHaveProperty("failureMessage");
 
-      if ("transactions" in item) {
-        expect(item.transactions).toBeInstanceOf(Array);
-        item.transactions.forEach((transaction) => {
-          expect(transaction).toHaveProperty("blockchainType");
-          expect(transaction).toHaveProperty("UnsignedTransaction");
-        });
-      }
-    });
+    // Type guard to check if result is SmartLinkUserTxInteraction
+    if ("transactions" in result) {
+      expect(result.transactions).toBeInstanceOf(Array);
+      result.transactions.forEach((transaction) => {
+        expect(transaction).toHaveProperty("blockchainType");
+        expect(transaction).toHaveProperty("UnsignedTransaction");
+      });
+    }
   });
 });
