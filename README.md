520cff7814c9b9b0bb7d802e24facffad50912c9
mayu <mayuran.ganesathas@gmail.com>
2025-04-10 14:24:25 -0700

    update utils

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 Smart Link Template
 
 
-This is a generated template based on your config from Notifi Admin Panels
+This is a generated template based on your config from Notifi Admin Panel
+
+
+NPM INIT SMARTLINK DEVELOPMENT
+-Generate Inputs for smart link 
+
 
---
+++ tests/unit/index.test.ts
@@ -0,0 +1,23 @@
+import parse from "../../src/parse";
+
+console.log("Tests Failing? Make sure you've run `npm run init-smart-link`");
+describe("parse function", () => {
+  it("should return SmartLinkActionResult when no transactions are present", () => {
+    const result = parse();
+    expect(result).toHaveProperty("successMessage");
+    expect(result).toHaveProperty("failureMessage");
+    expect(result).not.toHaveProperty("transactions");
+  });
+
+  it("should return SmartLinkUserTxInteraction when transactions are present", () => {
+    const input = {
+      transactions: [
+        { blockchainType: "ARBITRUM", UnsignedTransaction: "0x123" },
+      ],
+    };
+    const result = parse(input);
+    expect(result).toHaveProperty("successMessage");
+    expect(result).toHaveProperty("failureMessage");
+    expect(result).toHaveProperty("transactions");
+  });
+});
